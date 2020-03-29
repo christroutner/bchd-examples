9e6d588e9993b544d46a52eeff349a4e5df677d1
Chris Troutner <chris.troutner@gmail.com>
2020-03-29 13:20:22 -0700

    switching to the npm library

--- index.js
+++ index.js
@@ -2,16 +2,17 @@
   Examples for using the gRPC interface for BCHD in a node.js app.
 */
 
-const GrpcClient = require('../grpc-bchrpc-node').GrpcClient
+// const GrpcClient = require('../grpc-bchrpc-node').GrpcClient
+const GrpcClient = require('grpc-bchrpc-node').GrpcClient
 const grpc = new GrpcClient()
 
 async function runTest () {
   try {
-    const txid = "11556da6ee3cb1d14727b3a8f4b37093b6fecd2bc7d577a02b4e98b7be58a7e8";
+    const txid = '11556da6ee3cb1d14727b3a8f4b37093b6fecd2bc7d577a02b4e98b7be58a7e8'
 
-    const res = await grpc.getRawTransaction({ hash: txid, reversedHashOrder: true });
+    const res = await grpc.getRawTransaction({ hash: txid, reversedHashOrder: true })
 
-    console.log(`res: `, res)
+    console.log('res: ', res)
   } catch (err) {
     console.error('Error: ', err)
   }
