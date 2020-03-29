5d5488a93efd9d66ea50600f4bb742689d09fc4c
Chris Troutner <chris.troutner@gmail.com>
2020-03-29 13:02:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+  Examples for using the gRPC interface for BCHD in a node.js app.
+*/
+
+async function runTest () {
+  try {
+    console.log('hello world')
+  } catch (err) {
+    console.error('Error: ', err)
+  }
+}
+
+runTest()
